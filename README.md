38bb4b9695edf98cd8686c6f816cd3ca897f0631
Gonkers <github@gonkers.net>
2020-08-18 01:53:36 -0400

    Initial commit TODO:   add lots of unit tests   add benchmarks?   add samples?

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # BitCollection
 Inspired by VitVector32 and BitArray, I created something of a cross between the two. 👹
+
+> I've got a bad feeling about this.
+ - Princess Leia, Obi-Wan Kenobi, Anakin Skywalker, Luke Skywalker, C-3PO, Han Solo, BB-8, Lando Calrissian
+
+Pull requests welcome, use at your own risk, the Star Wars universe has warned you.
+
+The hope for this was to create an efficient (small), yet well defined, JWT claim that could
+store user permissions or other flags. For instance when base64 encoding 128 different flags
+(bits) it can be stored in a user claim as short as 22 characters. If each resource knows that
+bit 7 is the permission to create new users you can use the claim to enable and disable user
+controls to without needing to make calls to the user permissions store, etc.
+
+Feel free to fork or submit a pull request if you feel you have an improvement. If you find a
+bug file an issue and I'll  try to respond in a timely manner.
---
+++ src/Gonkers.BitCollection/BitCollection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gonkers.Bits
+{
+    public readonly struct BitCollection : IReadOnlyList<bool>
+    {
+        internal const byte BitsInAByte = 8;
+        public BitCollection(string base64) : this(Convert.FromBase64String(base64)) { }
+
+        public BitCollection(ReadOnlySpan<byte> bytes) : this(bytes.ToArray()) { }
+
+        public BitCollection(byte[] bytes)
+        {
+            if (bytes == null) throw new ArgumentNullException(nameof(bytes));
+            _bytes = new byte[bytes.Length];
+            bytes.CopyTo(_bytes, 0);
+            Count = bytes.Length * BitsInAByte;
+        }
+
+        private readonly byte[] _bytes;
+
+        public int Count { get; }
+
+        public int Slice(int start, int length)
+        {
+            if (start < 0 || start > MaxIndex)
+                throw new IndexOutOfRangeException($"The {nameof(start)} must be from 0 to {MaxIndex}.");
+
+            if (length > 32)
+                throw new IndexOutOfRangeException($"The {nameof(length)} must be from 1 to 32.");
+
+            if (length == 0) return this[start] ? 1 : 0;
+
+            var longBytes = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            var startByte = start / BitsInAByte;
+            var startOffset = start - startByte * BitsInAByte;
+            var endByte = (start + length - 1) / BitsInAByte;
+            var endOffset = 64 - (startOffset + length);
+
+            for (int i = 0; startByte + i <= endByte; i++)
+            {
+                longBytes[i] = _bytes[startByte + i];
+            }
+
+            // convert the bytes to a long, then trim extra bits on both ends
+            var value = BitConverter.ToUInt64(longBytes) << endOffset >> (endOffset + startOffset);
+            return unchecked((int)value);
+        }
+
+        public ReadOnlySpan<byte> AsReadOnlySpan() => new ReadOnlySpan<byte>(_bytes);
+
+        private int MaxIndex => Count - 1;
+
+        public bool this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > MaxIndex)
+                    throw new IndexOutOfRangeException($"The {nameof(index)} must be from 0 to {MaxIndex}");
+
+                // Determine the byte in the array which contains the indexed bit
+                // Shift the bits in the byte so the requested bit index is in the first bit position
+                // Check if the first bit is 1 or 0 and return true or false respectively
+                return ((byte)(_bytes[index / BitsInAByte] >> (index % BitsInAByte)) & 0b0000_0001) == 0b0000_0001;
+            }
+            set // considering making this read only... Write once, read many ?
+            {
+                if (index < 0 || index > MaxIndex)
+                    throw new IndexOutOfRangeException($"The {nameof(index)} must be from 0 to {MaxIndex}");
+
+                // Locate the bit we need to change and create a bitmask
+                var bitmask = (byte)(0b0000_0001 << (index % BitsInAByte));
+
+                if (value)
+                {
+                    // Using the bitmask set the indexed bit value to true
+                    _bytes[index / BitsInAByte] |= bitmask;
+                }
+                else
+                {
+                    // Using the inverted bitmask set the indexed bit value to false
+                    _bytes[index / BitsInAByte] &= (byte)~bitmask;
+                }
+            }
+        }
+
+        public string ToBase64String() => Convert.ToBase64String(_bytes);
+        public override string ToString() => ToBase64String();
+        public IEnumerator<bool> GetEnumerator() => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+    }
+}
